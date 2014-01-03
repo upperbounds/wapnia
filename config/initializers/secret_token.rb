d6a527edbb62a9c5cbbd7bb2fc7d55a55f17d4e7
Colin Flanagan <colin.flanagan-morgan@modaoperandi.com>
2014-01-03 12:09:00 -0500

    rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wapnia::Application.config.secret_token = 'ae35d304a034b79aa3a25bc7e175f8339da30a1e47762156bc3bec1d8bd39c2178073e084c27ae9fd5809bdb80428e2601007a8158cbd561d29deeaf0a09716c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wapnia::Application.config.session_store :cookie_store, key: '_wapnia_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wapnia::Application.config.session_store :active_record_store
